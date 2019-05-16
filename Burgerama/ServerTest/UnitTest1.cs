041d514b94a9662c8db4ddee3b4c905636ad30d4
Oliver <oliversiedler@gmx.de>
2019-05-16 18:46:06 +0200

    added projects

---
+++ Burgerama/ServerTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
