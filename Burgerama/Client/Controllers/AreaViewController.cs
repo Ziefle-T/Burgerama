b2c9c3100db40f3db9c4d0e8d0cf2f6baca18109
Tobias Ziefle <tobiasziefle@googlemail.com>
2019-06-15 12:55:52 +0200

    Added AreaView+ViewModel+Controller

--- Burgerama/Client/Controllers/AreaViewController.cs
+++ Burgerama/Client/Controllers/AreaViewController.cs
@@ -1,13 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Client.Server.Models;
+using Client.Server.Services;
 using Client.ViewModels;
 
 namespace Client.Controllers
 {
     class AreaViewController : ActionAreaController<AreaViewModel>
     {
+        private IAreaService mAreaService;
+
+        public AreaViewController(
+            IAreaService areaService
+
+        ) : base()
+        {
+            mAreaService = areaService;
+        }
+        public override AreaViewModel Initialize()
+        {
+            var retVal = base.Initialize();
+
+            retVal.Areas = new ObservableCollection<Area>(mAreaService.GetAll());
+            return retVal;
+        }
+        public override bool CanExecuteDeleteCommand(object obj)
+        {
+            return mViewModel.SelectedArea != null;
+        }
+        public override bool CanExecuteEditCommand(object obj)
+        {
+            return mViewModel.SelectedArea != null;
+        }
+        public override bool CanExecuteNewCommand(object obj)
+        {
+            return true;
+        }
+        public override bool CanExecuteSaveCommand(object obj)
+        {
+            return true;
+        }
+
+        public override void ExecuteDeleteCommand(object obj)
+        {
+            if (mViewModel.SelectedArea == null)
+            {
+                ShowMessage("Nichts zum löschen ausgewählt.");
+                return;
+            }
+
+            mAreaService.Delete(mViewModel.SelectedArea.Id);
+            ResetView();
+        }
+
+        public override void ExecuteEditCommand(object obj)
+        {
+            mViewModel.EditingArea = mViewModel.SelectedArea;
+        }
+
+        public override void ExecuteNewCommand(object obj)
+        {
+            mViewModel.EditingArea = new Area()
+            {
+                Id = 0,
+                Plz = 0,
+                Name = ""
+
+            };
+        }
+
+        public override void ExecuteSaveCommand(object obj)
+        {
+            if (mViewModel.EditingArea == null)
+            {
+                ShowMessage("Es gibt nichts zu speichern.");
+                return;
+            }
+
+            if (mViewModel.EditingArea.Id == 0)
+            {
+                mAreaService.Add(mViewModel.EditingArea);
+            }
+            else
+            {
+                mAreaService.UpdateArea(mViewModel.EditingArea.Id, mViewModel.EditingArea);
+            }
+
+            ResetView();
+        }
+        private void ResetView()
+        {
+            mViewModel.SelectedArea = null;
+            mViewModel.EditingArea = null;
+            mViewModel.Areas = new ObservableCollection<Area>(mAreaService.GetAll());
+        }
     }
 }
--- Burgerama/Client/ViewModels/AreaViewModel.cs
+++ Burgerama/Client/ViewModels/AreaViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Client.Server.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
     class AreaViewModel : ActionAreaViewModel
     {
+        public ObservableCollection<Area> Areas { get; set; }
+        public Area SelectedArea { get; set; }
+        public Area EditingArea;
+        public ICommand AddAreaCommand { get; set; }
+        public ICommand RemoveAreaCommand{ get; set; }
     }
 }
